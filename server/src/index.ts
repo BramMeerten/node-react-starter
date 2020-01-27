262a139e330d640c62978aa139369d7e7d6b19a9
BramMeerten <bram.mee@gmail.com>
2020-01-27 23:09:10 +0100

    Init

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import {Application} from 'express';
+
+const express = require('express');
+
+const BASE_PATH = '/api';
+const PORT = 8080;
+const app: Application = express();
+
+app.get(`${BASE_PATH}/hello`, (req: any, res: any) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Doc Editor is listening on port ${PORT}`);
+});
